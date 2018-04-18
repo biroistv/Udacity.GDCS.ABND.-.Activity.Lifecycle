8f3aeb765d94368da04cf1038da9fca3214bb573
Biró István <biroistv@outlook.hu>
2018-04-18 11:14:32 +0200

    adding lifeciycle log messages

--- app/src/main/java/com/example/biro/udacitygdcsabnd_activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/biro/udacitygdcsabnd_activitylifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.biro.udacitygdcsabnd_activitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setLogMessage("onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setLogMessage("onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setLogMessage("onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setLogMessage("onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        setLogMessage("onRestart");
+    }
+
+    private void setLogMessage(String str)
+    {
+        Log.v(this.toString(), str);
     }
 }
